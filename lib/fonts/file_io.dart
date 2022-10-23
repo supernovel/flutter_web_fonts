75c976d11672f5fdaf994f6b49af98dcf616400a
supernovel <ekfueh@naver.com>
2022-10-23 17:01:13 +0900

    fix: missing async keyword

--- lib/fonts/file_io.dart
+++ lib/fonts/file_io.dart
@@ -8,10 +8,11 @@ import 'dart:typed_data' show ByteData;
 /// Stubbed out version of saveFontToDeviceFileSystem from
 /// `file_io_desktop_and_mobile.dart`.
 Future<void> saveFontToDeviceFileSystem(String name, List<int> bytes,
-        {String? ext}) =>
+        {String? ext}) async =>
     throw Exception("Not supported platform");
 
 /// Stubbed out version of loadFontFromDeviceFileSystem from
 /// `file_io_desktop_and_mobile.dart`.
-Future<ByteData> loadFontFromDeviceFileSystem(String name, {String? ext}) =>
+Future<ByteData> loadFontFromDeviceFileSystem(String name,
+        {String? ext}) async =>
     throw Exception("Not supported platform");
